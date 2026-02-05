a3bffa16166d5db46ebe932d76dc7a263a1892ab
HyunhSo <blueyalu@naver.com>
2026-02-05 14:04:37 +0900

    Set up UE5 ModularGameplay starter project

---
+++ Source/MyModularGame/MyModularGame.Build.cs
@@ -0,0 +1,26 @@
+using UnrealBuildTool;
+
+public class MyModularGame : ModuleRules
+{
+    public MyModularGame(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "ModularGameplay",
+            "GameFeatures",
+            "GameplayTags"
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+            "ModularGameplayActors"
+        });
+    }
+}
---
+++ Source/MyModularGame/MyModularGame.cpp
@@ -0,0 +1,4 @@
+#include "MyModularGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, MyModularGame, "MyModularGame");
